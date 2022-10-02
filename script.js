5939e6c3bd803b8e0c3676d744db07714c2bd026
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2022-10-02 01:29:31 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+document.querySelector('.add-photo').addEventListener('click', () => {
+    if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+        !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+        alert('User Media API not supported.');
+        return;
+      }
+      
+      var constraints = {
+        video: true
+      };
+    
+      getUserMedia(constraints, function (stream) {
+        var mediaControl = document.querySelector('video');
+       
+        document.querySelector('.show-video').classList.remove('d-none')
+        document.querySelector('.back').classList.remove('d-none')
+        document.querySelector('.form').classList.add('d-none')
+        document.querySelector('.add-photo').classList.add('d-none')
+        if ('srcObject' in mediaControl) {
+          mediaControl.srcObject = stream;
+        } else if (navigator.mozGetUserMedia) {
+          mediaControl.mozSrcObject = stream;
+        } else {
+          mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+        }
+        theStream = stream;
+      }, function (err) {
+        alert('Error: ' + err);
+      });
+});
+  
+var theStream;
+
+document.querySelector('.take-photo').addEventListener('click', () => {
+    if (!('ImageCapture' in window)) {
+        alert('ImageCapture is not available');
+        return;
+      }
+      
+      if (!theStream) {
+        alert('Grab the video stream first!');
+        return;
+      }
+      
+      var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+    
+      theImageCapturer.takePhoto()
+        .then(blob => {
+          var theImageTag = document.getElementById("imageTag");
+          theImageTag.src = URL.createObjectURL(blob);
+        })
+        .catch(err => alert('Error: ' + err));
+});
+
+function getUserMedia(options, successCallback, failureCallback) {
+    var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    if (api) {
+      return api.bind(navigator)(options, successCallback, failureCallback);
+    }
+  }
+
+  
+ 
+  
+ 
+  
